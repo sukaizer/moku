201f57b012cea53e1a098d9d997bfa04c46d0486
Alexandre Ciorascu <alexandre.ciorascu@u-psud.fr>
2022-09-24 16:06:13 +0200

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// let f = function (text = "hello") {
+//   let el = document.createElement("div");
+//   el.setAttribute("class", "fancy");
+
+//   var text = document.createTextNode(text);
+//   el.appendChild(text);
+//   document.body.appendChild(el);
+// };
+
+// f("you can put whatever you wish for");
